c3adb21068d2ede8f4c44d0934bc9e5a727a3930
SambitSenapati-78 <60101251+SambitSenapati-78@users.noreply.github.com>
2022-05-01 12:06:01 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,20 @@
+module.exports.getDate = function() {
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  return today.toLocaleDateString("hi-IN", options);
+}
+
+module.exports.getDay = function() {
+  let today = new Date();
+
+  let options = {
+    weekday: "long"
+  };
+  return today.toLocaleDateString("hi-IN", options);
+}
+console.log(module.exports);
